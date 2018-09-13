2c5c5ce2ad16d5aa94a00d0fcac60490213d4f6d
mitra1759 <42783913+mitra1759@users.noreply.github.com>
2018-09-13 14:33:50 -0500

    some updates

--- app/src/main/java/com/example/rc211/mindlessgame/MainActivity.java
+++ app/src/main/java/com/example/rc211/mindlessgame/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.rc211.mindlessgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button star=  findViewById(R.id.start);
+    private ImageView pendulum = findViewById(R.id.pendulum);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+    star.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v){
+            System.out.println("fdg");
+        }
+    });
 }
