f39519afd9c56d3011d00bf1cfcdca3a154f93b3
mitra1759 <42783913+mitra1759@users.noreply.github.com>
2018-09-14 13:58:58 -0500

    fixed buttons

--- app/src/main/java/com/example/rc211/mindlessgame/MainActivity.java
+++ app/src/main/java/com/example/rc211/mindlessgame/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.rc211.mindlessgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private Button star=  findViewById(R.id.start);
     private ImageView pendulum = findViewById(R.id.pendulum);
     @Override
@@ -14,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        star.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
 
-    star.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v){
-            System.out.println("fdg");
-        }
-    });
+
 }
